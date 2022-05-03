e291d0789787d7e1fbdd22961081c0d64c599007
Estuardo Vasquez <vasquezalvarado@hotmail.es>
2022-05-03 09:47:08 -0600

    Actualización de programa
    EL programa funciona bien.

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ print("Estuardo Alejandro Vpasquez")
 print("2495420")
 print("Ingeniería en Informática y Sistemas")
 print("Primer ciclo")
+
+animal_fav = "Gatos"
+
+animal = input("Ingrese un animal: ")
+if animal == animal_fav:
+    print(f"A mí también me gustan los {animal_fav}")
+
+else:
+    print(f"Es un animal interesante, pero me gustan más los {animal_fav}")
